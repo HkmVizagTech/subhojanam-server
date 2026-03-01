9a6a4fd42bdfcf4b0f4c02ca00a673439db18394
sai kiran <saikiran@sais-MacBook-Air.local>
2026-03-01 11:44:36 +0530

    done the register bug

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -51,11 +51,12 @@ const adminSchema = new mongoose.Schema(
 );
 
 
-adminSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 
